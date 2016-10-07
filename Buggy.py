d306bd62ea9cfbd55fcdcd4a36ff1abb0e384b87
Anton Nikitin <antonnikitin@live.co.uk>
2016-10-07 14:01:27 +0100

    Fixed commit issues and made code more readable

--- Buggy.py
+++ Buggy.py
@@ -1,35 +1,27 @@
 #Implemenation of a bubble sort
-#This code complies fine but has some errors that will prevent it from running properly
-#Find these errors and correct them, writing a comment explaining what the error was.
-
-
-def sortList(listToSort):
+ #This code complies fine but has some errors that will prevent it from running properly
+ #Find these errors and correct them, writing a comment explaining what the error was.
+ 
+ 
+  def sortList(listToSort):
+  
 	tempStore = 0
     numberOfPasses = len(listToSort)
-	
+ +	
     for i in range(numberOfPasses):
-<<<<<<< HEAD
-	
+ +	
         for i in range(len(listToSort)):
-		
-=======
-        for i in range(len(listToSort)):
->>>>>>> 6a8a05472c8c70ca9ed2972d63690535ae822c2d
+ +		
             if listToSort[i] >= listToSort[i + 1]:
                 tempStoreOne = listToSort[i + 1]
                 listToSort[i + 1] = listToSort[i]
                 listToSort[i] = tempStoreOne
-				
+				  
     return listToSort
-	
-def printContents(listToPrint):
+	  
+  def printContents(listToPrint):
+	for i in listToPrint:      
+		pass
 
-    for i in listToPrint:
-<<<<<<< HEAD
-        passs
-=======
-        print i
->>>>>>> 6a8a05472c8c70ca9ed2972d63690535ae822c2d
-		
-sortedList = sortList(list({3,45,212,31,53,4}))
-printContents(sortedList)
+  sortedList = sortList(list({3,45,212,31,53,4}))
+  printContents(sortedList) 
