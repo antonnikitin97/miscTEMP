7bcb1ba38cc166b55a527e8ffa609f4a10ba1cd4
Anton Nikitin <antonnikitin@live.co.uk>
2016-10-07 17:30:34 +0100

    NonBuggy.py

---
+++ NonBuggy.py
@@ -0,0 +1,27 @@
+#Implemenation of a bubble sort
+#This code complies fine but has some errors that will prevent it from running properly
+#Find these errors and correct them, writing a comment explaining what the error was.
+ 
+ 
+  def sortList(listToSort):
+  
+    tempStore = 0
+    numberOfPasses = len(listToSort) - 1
+ 	
+    for i in range(numberOfPasses):
+ 	
+        for i in range(len(listToSort) - 1):
+ 		
+            if listToSort[i] > listToSort[i + 1]:
+                tempStoreOne = listToSort[i + 1]
+                listToSort[i + 1] = listToSort[i]
+                listToSort[i] = tempStoreOne
+				  
+    return listToSort
+	  
+  def printContents(listToPrint):
+	for i in listToPrint:      
+		print i
+
+  sortedList = sortList(list({3,45,212,31,53,4}))
+  printContents(sortedList) 
