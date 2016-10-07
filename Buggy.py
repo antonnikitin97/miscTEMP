0a2aafc3b77fb234e00971d4f285ab5425d8892b
Anton Nikitin <antonnikitin@live.co.uk>
2016-10-07 13:49:39 +0100

    Made code more readable

--- Buggy.py
+++ Buggy.py
@@ -1,19 +1,27 @@
 #Implemenation of a bubble sort
-tempStoreOne = 0
+#This code complies fine but has some errors that will prevent it from running properly
+#Find these errors and correct them, writing a comment explaining what the error was.
+
+
 def sortList(listToSort):
-    # Total passes = length of list - 1
+	tempStore = 0
     numberOfPasses = len(listToSort)
+	
     for i in range(numberOfPasses):
-        # This will cause an indexArrayOutOfBounds (need to minus 1 from the length)
+	
         for i in range(len(listToSort)):
-            # Should be > and not >=
+		
             if listToSort[i] >= listToSort[i + 1]:
                 tempStoreOne = listToSort[i + 1]
                 listToSort[i + 1] = listToSort[i]
                 listToSort[i] = tempStoreOne
+				
     return listToSort
+	
 def printContents(listToPrint):
+
     for i in listToPrint:
-        print i
+        passs
+		
 sortedList = sortList(list({3,45,212,31,53,4}))
 printContents(sortedList)
